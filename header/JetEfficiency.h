9d4fe615b7c949615de17713117c9c47bcbe80db
Nicholas Barnett <133896112+thenicholasbarnett@users.noreply.github.com>
2026-05-11 00:13:07 -0500

    Add files via upload

---
+++ header/JetEfficiency.h
@@ -0,0 +1,141 @@
+#ifndef JETEFFICIENCY_H
+#define JETEFFICIENCY_H
+ 
+#include "TFile.h"
+#include "TH1F.h"
+#include "TGraphAsymmErrors.h"
+#include "TString.h"
+
+#include <vector>
+#include <stdexcept>
+
+#include "Binning.h"
+#include "TriggerMap_PbPb_MC.h"
+#include "JetSpectraHistograms.h"
+
+inline void GetHistograms(TFile* fi, const BinningStruct& bins, JetSpectraStruct& hists){
+
+    auto GetTH1F = [&](const TString& hname) -> TH1F* {
+        TH1F* h = (TH1F*)fi->Get(hname);
+        if(!h){throw std::runtime_error(Form("ERROR: Could not find histogram '%s' in file", hname.Data()));}
+        return h;
+    };
+
+    const TString ji = "jet_inclusive/hjtpt";
+    const TString jh = "jet_hibin/hjtpt";
+    const std::size_t nEta = bins.etaBins.size();
+    const std::size_t nhiBin = bins.hiBins.size();
+
+    // looping through eta bins
+    for(std::size_t b=0; b<nEta; b++){
+        const auto& etaBin = bins.etaBins[b];
+        hists.ljtpt_eta[b] = GetTH1F(ji + etaBin.shortName);
+
+        // looping through HLT
+        for (std::size_t t=0; t<nHLT; t++){
+            TString hltShort = GetHLTShortName(sHLTrigs[t]);
+
+            // looping through dR matching types
+            for (int m=0; m<JetSpectraStruct::kNMatchTypes; m++){
+                auto matchType = static_cast<JetSpectraStruct::MatchType>(m);
+                hists.hlt_ljtpt_eta[matchType][b][t] = GetTH1F(ji + JetSpectraStruct::MatchSuffix(matchType) + etaBin.shortName + hltShort);
+            }
+        }
+
+        // looping through L1T
+        for (std::size_t t = 0; t < nL1T; t++){
+            TString l1Short = GetL1ShortName(sL1Trigs[t]);
+            hists.l1_ljtpt_eta[b][t] = GetTH1F(ji + etaBin.shortName + l1Short);
+        }
+
+        // looping through hiBins
+        for (std::size_t hb=0; hb<nhiBin; hb++){
+            const auto& hiBin = bins.hiBins[hb];
+            hists.ljtpt_hibin[b][hb] = GetTH1F(jh + etaBin.shortName + hiBin.shortName);
+
+            for (std::size_t t=0; t<nHLT; t++){
+                TString hltShort = GetHLTShortName(sHLTrigs[t]);
+
+                for (int m=0; m<JetSpectraStruct::kNMatchTypes; m++){
+                    auto matchType = static_cast<JetSpectraStruct::MatchType>(m);
+                    hists.hlt_ljtpt_hibin[matchType][b][t][hb] = GetTH1F(jh + JetSpectraStruct::MatchSuffix(matchType) + etaBin.shortName + hltShort + hiBin.shortName);
+                }
+            }
+
+            for (std::size_t t=0; t<nL1T; t++) {
+                TString l1Short = GetL1ShortName(sL1Trigs[t]);
+                hists.l1_ljtpt_hibin[b][t][hb] = GetTH1F(jh + etaBin.shortName + l1Short + hiBin.shortName);
+            }
+        }
+    }
+}
+
+struct JetEfficiencyOutputStruct{
+    
+    enum EfficiencyType{
+        kFull = 0, // HLT+L1seed+minBias/minBias
+        kRelative = 1, // HLT+L1seed+minBias/L1seed+minBias
+        kNEfficiencyTypes = 2
+    };
+
+    static TString EfficiencyName(EfficiencyType effType){
+        if(effType == kFull){return "full";}
+        if(effType == kRelative){return "relative";}
+        return "unknown";
+    }
+
+    // [MatchType][EfficiencyType][Eta][HLT]
+    std::vector<std::vector<std::vector<std::vector<TGraphAsymmErrors*>>>> jetEfficiencies_inclusive;
+
+    // [MatchType][EfficiencyType][Eta][hiBin][HLT]
+    std::vector<std::vector<std::vector<std::vector<std::vector<TGraphAsymmErrors*>>>>> jetEfficiencies_hibin;
+
+    JetEfficiencyOutputStruct() = default;
+    JetEfficiencyOutputStruct(const BinningStruct& bins){Init(bins);}
+
+    void Init(const BinningStruct& bins){
+        const std::size_t nEta = bins.etaBins.size();
+        const std::size_t nhiBin = bins.hiBins.size();
+
+        jetEfficiencies_inclusive.resize(JetSpectraStruct::kNMatchTypes,std::vector<std::vector<std::vector<TGraphAsymmErrors*>>>(kNEfficiencyTypes,std::vector<std::vector<TGraphAsymmErrors*>>(nEta, std::vector<TGraphAsymmErrors*>(nHLT, nullptr))));
+        jetEfficiencies_hibin.resize(JetSpectraStruct::kNMatchTypes,std::vector<std::vector<std::vector<std::vector<TGraphAsymmErrors*>>>>(kNEfficiencyTypes,std::vector<std::vector<std::vector<TGraphAsymmErrors*>>>(nEta,std::vector<std::vector<TGraphAsymmErrors*>>(nhiBin,std::vector<TGraphAsymmErrors*>(nHLT, nullptr)))));
+    }
+    
+    // generates efficiencies from jet spectra
+    void ComputeEfficiencies(const JetSpectraStruct& hists, const BinningStruct& bins){
+        const std::size_t nEta = bins.etaBins.size();
+        const std::size_t nhiBin = bins.hiBins.size();
+
+        // looping over dR matching type
+        for(int m=0; m<JetSpectraStruct::kNMatchTypes; m++){
+            auto matchType = static_cast<JetSpectraStruct::MatchType>(m);
+
+            // looping over efficiency type
+            for(int e=0; e<kNEfficiencyTypes; e++){
+                auto effType = static_cast<EfficiencyType>(e);
+
+                // looping over eta
+                for(std::size_t b=0; b<nEta; b++){
+
+                    // looping over HLT
+                    for(std::size_t t=0; t<nHLT; t++){
+
+                        // ternary operator
+                        TH1F* denom_eta = (effType == kFull) ? hists.ljtpt_eta[b] : hists.l1_ljtpt_eta[b][L1SeedHLT[t]];
+
+                        // performing ratio to get jet efficiency
+                        jetEfficiencies_inclusive[matchType][effType][b][t] = new TGraphAsymmErrors(hists.hlt_ljtpt_eta[matchType][b][t], denom_eta, "cl=0.683 b(1,1) mode");
+
+                        // looping over hiBin
+                        for(std::size_t hb=0; hb<nhiBin; hb++){
+                            TH1F* denom_hibin = (effType == kFull) ? hists.ljtpt_hibin[b][hb] : hists.l1_ljtpt_hibin[b][L1SeedHLT[t]][hb];
+                            jetEfficiencies_hibin[matchType][effType][b][hb][t] = new TGraphAsymmErrors(hists.hlt_ljtpt_hibin[matchType][b][t][hb], denom_hibin, "cl=0.683 b(1,1) mode");
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+#endif
--- header/JetSpectraHistograms.h
+++ header/JetSpectraHistograms.h
@@ -59,10 +59,36 @@ struct JetSpectraStruct{
     std::vector<std::vector<std::vector<std::vector<TH1F*>>>> hlt_ljtpt_hibin;
 
     JetSpectraStruct() = default;
-    JetSpectraStruct(const BinningStruct& bins) {Init(bins);}
+    JetSpectraStruct(const BinningStruct& bins){
+        InitAxes(bins);
+        InitHistograms(bins);
+    }
+    JetSpectraStruct(const BinningStruct& bins, bool allocate){
+        InitAxes(bins);
+        if(allocate){InitHistograms(bins);}
+    }
+    void InitAxes(const BinningStruct& bins){
+        const std::size_t nEta = bins.etaBins.size();
+        const std::size_t nhiBin = bins.hiBins.size();
+        // [Eta]
+        ljtpt_eta.resize(nEta, nullptr);
+        // [Eta][L1]
+        l1_ljtpt_eta.resize(nEta,std::vector<TH1F*>(nL1T, nullptr));
+        // [MatchType][Eta][HLT]
+        hlt_ljtpt_eta.resize(kNMatchTypes,std::vector<std::vector<TH1F*>>(nEta,std::vector<TH1F*>(nHLT, nullptr)));
+        // [Eta][hiBin]
+        ljtpt_hibin.resize(nEta,std::vector<TH1F*>(nhiBin, nullptr));
+        // [Eta][L1][hiBin]
+        l1_ljtpt_hibin.resize(nEta,std::vector<std::vector<TH1F*>>(nL1T,std::vector<TH1F*>(nhiBin, nullptr)));
+        // [MatchType][Eta][HLT][hiBin]
+        hlt_ljtpt_hibin.resize(kNMatchTypes,std::vector<std::vector<std::vector<TH1F*>>>(nEta,std::vector<std::vector<TH1F*>>(nHLT,std::vector<TH1F*>(nhiBin, nullptr))));
+    }
 
-    void Init(const BinningStruct& bins) {
+    void InitHistograms(const BinningStruct& bins) {
         TString hname;
+ 
+        const std::size_t nEta   = bins.etaBins.size();
+        const std::size_t nhiBin = bins.hiBins.size();
         
         vz = MakeTH1F("hvz", bins.vz);
         vz_unpassed = MakeTH1F("hvz_unpassed", bins.vz);
@@ -72,66 +98,46 @@ struct JetSpectraStruct{
         ppvF = MakeTH1I("hppvF", bins.trig);
         pphfF = MakeTH1I("hpphfF", bins.trig);
 
-        const std::size_t nEta   = bins.etaBins.size();
-        const std::size_t nhiBin = bins.hiBins.size();
-
-        // [Eta]
-        ljtpt_eta.resize(nEta, nullptr);
-
-        // [Eta][L1]
-        l1_ljtpt_eta.resize(nEta,std::vector<TH1F*>(nL1T, nullptr));
-
-        // [MatchType][Eta][HLT]
-        hlt_ljtpt_eta.resize(kNMatchTypes,std::vector<std::vector<TH1F*>>(nEta,std::vector<TH1F*>(nHLT, nullptr)));
-
-        // [Eta][hiBin]
-        ljtpt_hibin.resize(nEta,std::vector<TH1F*>(nhiBin, nullptr));
-
-        // [Eta][L1][hiBin]
-        l1_ljtpt_hibin.resize(nEta,std::vector<std::vector<TH1F*>>(nL1T,std::vector<TH1F*>(nhiBin, nullptr)));
-
-        // [MatchType][Eta][HLT][hiBin]
-        hlt_ljtpt_hibin.resize(kNMatchTypes,std::vector<std::vector<std::vector<TH1F*>>>(nEta,std::vector<std::vector<TH1F*>>(nHLT,std::vector<TH1F*>(nhiBin, nullptr))));
-
-        for (std::size_t b=0; b<nEta; b++) {
+        
+        for (std::size_t b=0; b<nEta; b++){
             const auto& etaBin = bins.etaBins[b]; 
 
             hname = "hjtpt" + etaBin.shortName;
             ljtpt_eta[b] = MakeTH1F(hname, bins.pt);
 
-            for (std::size_t t = 0; t < nHLT; t++) {
+            for (std::size_t t = 0; t < nHLT; t++){
                 TString hltShort = GetHLTShortName(sHLTrigs[t]);
 
-                for (int m = 0; m < kNMatchTypes; m++) {
+                for (int m=0; m<kNMatchTypes; m++){
                     MatchType matchType = static_cast<MatchType>(m);
                     hname = "hjtpt" + MatchSuffix(matchType) + etaBin.shortName + hltShort;
                     hlt_ljtpt_eta[matchType][b][t] = MakeTH1F(hname, bins.pt);
                 }
             }
 
-            for (std::size_t t = 0; t < nL1T; t++) {
+            for (std::size_t t=0; t<nL1T; t++){
                 TString l1Short = GetL1ShortName(sL1Trigs[t]);
                 hname = "hjtpt" + etaBin.shortName + l1Short;
                 l1_ljtpt_eta[b][t] = MakeTH1F(hname, bins.pt);
             }
 
-            for (std::size_t hb = 0; hb < nhiBin; hb++) {
+            for (std::size_t hb=0; hb<nhiBin; hb++){
                 const auto& hiBin = bins.hiBins[hb];
 
                 hname = "hjtpt" + etaBin.shortName + hiBin.shortName;
                 ljtpt_hibin[b][hb] = MakeTH1F(hname, bins.pt);
 
-                for (std::size_t t = 0; t < nHLT; t++) {
-                    TString hltShort =GetHLTShortName(sHLTrigs[t]);
+                for (std::size_t t=0; t<nHLT; t++){
+                    TString hltShort = GetHLTShortName(sHLTrigs[t]);
 
-                    for (int m=0; m<kNMatchTypes; m++) {
+                    for (int m=0; m<kNMatchTypes; m++){
                         MatchType matchType = static_cast<MatchType>(m);
                         hname = "hjtpt" + MatchSuffix(matchType) + etaBin.shortName + hltShort + hiBin.shortName;
                         hlt_ljtpt_hibin[matchType][b][t][hb] = MakeTH1F(hname, bins.pt);
                     }
                 }
                 
-                for (std::size_t t=0; t<nL1T; t++) {
+                for (std::size_t t=0; t<nL1T; t++){
                     TString l1Short = GetL1ShortName(sL1Trigs[t]);
                     hname = "hjtpt"+ etaBin.shortName+ l1Short+ hiBin.shortName;
                     l1_ljtpt_hibin[b][t][hb] = MakeTH1F(hname, bins.pt);
