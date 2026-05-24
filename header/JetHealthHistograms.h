70c53bb9bf3a4a967192c95954f9d9f0ff3196bf
Nicholas Barnett <133896112+thenicholasbarnett@users.noreply.github.com>
2026-05-24 16:53:06 +0200

    Add files via upload

--- header/JetHealthHistograms.h
+++ header/JetHealthHistograms.h
@@ -3,7 +3,9 @@
 
 #include "TH1F.h"
 #include "TH1I.h"
+#include "THnSparse.h"
 #include "TFile.h"
+#include "TMath.h"
 #include "TString.h"
 #include "Rtypes.h"
 
@@ -13,13 +15,14 @@
 #include "Binning.h"
 #include "HistUtilities.h"
 
-inline TH1F* MakeTH1F(const TString& hname, const AxisBins& bins) {
-    return new TH1F(hname, hname, bins.nBins, bins.lo, bins.hi);
-}
- 
-inline TH2F* MakeTH2F(const TString& hname, const AxisBins& xBins, const AxisBins& yBins) {
-    return new TH2F(hname, hname, xBins.nBins, xBins.lo, xBins.hi, yBins.nBins, yBins.lo, yBins.hi);
-}
+enum PFType{
+    CHF = 0,
+    NHF = 1,
+    CEF = 2,
+    NEF = 3,
+    MUF = 4,
+    PFTypes = 5
+};
 
 struct JetHealthStruct{
 
@@ -34,54 +37,36 @@ struct JetHealthStruct{
     TH1I* ppvF = nullptr;
     TH1I* pphfF = nullptr;
 
-    // kimematics
-    std::vector<TH1F*> pt;
-    std::vector<TH1F*> eta;
-    std::vector<TH1F*> phi;
+    // kimematics and PF fractions
+    THnSparseF* kin = nullptr;
+    THnSparseF* pf = nullptr;
 
-    // eta-phi maps
-    std::vector<TH2F*> etaphi;
+    JetHealthStruct(const BinningStruct& bins){InitHistograms(bins);}
     
-    // inclusive PF fractions
-    std::vector<TH1F*> CHF;
-    std::vector<TH1F*> NHF;
-    std::vector<TH1F*> CEF;
-    std::vector<TH1F*> NEF;
-    std::vector<TH1F*> MUF;
-
-    JetHealthStruct(const BinningStruct& bins) {
-        InitAxes(bins);
-        InitHistograms(bins);
-    }
-
-    void InitAxes(const BinningStruct& bins) {
-        const std::size_t nhiBin = bins.hiBins.size();
-        pt.resize(nhiBin, nullptr);
-        eta.resize(nhiBin, nullptr);
-        phi.resize(nhiBin, nullptr);
-        etaphi.resize(nhiBin, nullptr);
-        CHF.resize(nhiBin, nullptr);
-        NHF.resize(nhiBin, nullptr);
-        CEF.resize(nhiBin, nullptr);
-        NEF.resize(nhiBin, nullptr);
-        MUF.resize(nhiBin, nullptr);
-    }
-
-    void InitHistograms(const BinningStruct& bins) {
-        const std::size_t nhiBin = bins.hiBins.size();
+    void InitHistograms(const BinningStruct& bins){
+        vz = MakeTH1F("hvz", bins.vz);
+        vz_unpassed = MakeTH1F("hvz_unpassed", bins.vz);
+        hiBin = MakeTH1I("hhiBin", bins.hiBin);
+        nref = MakeTH1I("hnref", bins.nref);
+        pclustF = MakeTH1I("hpclustF", bins.trig);
+        ppvF = MakeTH1I("hppvF", bins.trig);
+        pphfF = MakeTH1I("hpphfF", bins.trig);
  
-        for (std::size_t hb = 0; hb < nhiBin; hb++) {
-            const TString& s = bins.hiBins[hb].shortName;
-            
-            pt[hb] = MakeTH1F("hjtpt"+s, bins.pt);
-            eta[hb] = MakeTH1F("hjteta"+s, bins.eta);
-            phi[hb] = MakeTH1F("hjtphi"+s, bins.phi);
-            etaphi[hb] = MakeTH2F("hjtetaphi"+s, bins.eta, bins.phi);
-            CHF[hb] = MakeTH1F("hjtCHF"+s, bins.pfFrac);
-            NEF[hb] = MakeTH1F("hjtNEF"+s, bins.pfFrac);
-            CEF[hb] = MakeTH1F("hjtCEF"+s, bins.pfFrac);
-            MUF[hb] = MakeTH1F("hjtMUF"+s, bins.pfFrac);
-            NHF[hb] = MakeTH1F("hjtNHF"+s, bins.pfFrac);
+        kin = MakeTHnSparseF("hjetkin", "pt:abseta:phi:hiBin", {bins.pt, bins.abseta, bins.phi, bins.hiBin});
+        const AxisBins pfType = {kNPFTypes, 0., (Float_t)PFTypes, "PF type (CHF/NHF/CEF/NEF/MUF)"};
+        pf  = MakeTHnSparseF("hjetpf",  "pfFrac:pfType:abseta:hiBin", {bins.pfFrac, pfType, bins.abseta, bins.hiBin});
+    }
+
+    void FillKin(const JetStruct<MAXNREF>::RecoMomenta& reco, Int_t j, Int_t hiBinVal, Float_t w = 1.0){
+        Double_t x[] = {reco.pt[j], reco.eta[j], reco.phi[j], (Double_t)hiBinVal};
+        kin->Fill(x, w);
+    }
+    
+    void FillPF(const JetStruct<MAXNREF>::RecoMomenta& reco, Int_t j, Int_t hiBinVal, Float_t w = 1.0){
+        const Float_t fracs[PFTypes] = {reco.pf.CHF[j], reco.pf.NHF[j], reco.pf.CEF[j], reco.pf.NEF[j], reco.pf.MUF[j]};
+        for (Int_t p = 0; p < PFTypes; p++) {
+            Double_t x[] = {fracs[p], p + 0.5, TMath::Abs(reco.eta[j]), (Double_t)hiBinVal};
+            pf->Fill(x, w);
         }
     }
 
@@ -94,19 +79,11 @@ struct JetHealthStruct{
         WriteAll(pclustF);
         WriteAll(ppvF);
         WriteAll(pphfF);
-        WriteAll(pt);
-        WriteAll(eta);
-        WriteAll(phi);
-        WriteAll(etaphi);
-        WriteAll(CHF);
-        WriteAll(NHF);
-        WriteAll(CEF);
-        WriteAll(MUF);
-        WriteAll(NEF);
+        if(kin){kin->Write();}
+        if(pf){pf->Write();}
         f->Close();
     }
 
-}
-
+};
 
 #endif
