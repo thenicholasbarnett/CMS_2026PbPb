e9d3acbe18282529410e658a8ed6e107dcb76d4e
Nicholas Barnett <133896112+thenicholasbarnett@users.noreply.github.com>
2026-05-25 11:28:40 +0200

    Add files via upload

---
+++ header/MyColors.h
@@ -0,0 +1,20 @@
+// "Sailing Boats Returning to Yabase", Lake Biwa, 1835, Utagawa Hiroshige https://github.com/BlakeRMills/MetBrewer?tab=readme-ov-file#hiroshige
+
+TColor HiroshigeLightRed = TColor::GetColor("#e76254");
+TColor HiroshigeOrange = TColor::GetColor("#ef8a47");
+TColor HiroshigeLightOrange = TColor::GetColor("#f7aa58");
+TColor HiroshigeYellow = TColor::GetColor("#ffd06f");
+TColor HiroshigeIceBlue = TColor::GetColor("#aadce0");
+TColor HiroshigeLightBlue = TColor::GetColor("#72bcd5");
+TColor HiroshigeBlue = TColor::GetColor("#528fad");
+TColor HiroshigeGrayBlue = TColor::GetColor("#376795");
+TColor HiroshigeNightBlue = TColor::GetColor("#1e466e");
+
+// "Mäda Primavesi", 1912, Gustav Klimt https://github.com/BlakeRMills/MetBrewer?tab=readme-ov-file#klimt 
+
+TColor KlimtPink = TColor::GetColor("#df9ed4");
+TColor KlimtRed = TColor::GetColor("#c93f55");
+TColor KlimtYellow = TColor::GetColor("#eacc62");
+TColor KlimtGreen = TColor::GetColor("#469d76");
+TColor KlimtBlue = TColor::GetColor("#3c4b99");
+TColor KlimtPurple = TColor::GetColor("#924099")
