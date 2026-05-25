c3a4702d9c7a5426ea342d2e063b488f798d62c9
Nicholas Barnett <133896112+thenicholasbarnett@users.noreply.github.com>
2026-05-25 07:54:07 +0200

    Update JetEfficiencyPlotting.h

--- header/JetEfficiencyPlotting.h
+++ header/JetEfficiencyPlotting.h
@@ -18,8 +18,8 @@
 #include "Binning.h"
 #include "JetEfficiency.h"
 #include "JetSpectraHistograms.h"
-#include "../header/JetTriggers_2026PbPb_MC.h"
-// #include "../header/JetTriggers_2025PbPb.h"
+#include "JetTriggers_2026PbPb_MC.h"
+// #include "JetTriggers_2025PbPb.h"
 
 struct PlotConfig{
     TString runNumber = "";
