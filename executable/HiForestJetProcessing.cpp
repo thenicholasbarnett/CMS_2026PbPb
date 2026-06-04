fbd1e0f73365dab9747b98ba9540e05d4b95529b
Nicholas Barnett <133896112+thenicholasbarnett@users.noreply.github.com>
2026-06-04 18:36:35 +0200

    Add files via upload

--- executable/HiForestJetProcessing.cpp
+++ executable/HiForestJetProcessing.cpp
@@ -3,22 +3,30 @@
 #include "TMath.h"
 #include "TDirectory.h"
 
+#include <chrono>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <stdexcept>
 
+#include "../header/JetSelection_PbPb.h"
+#include "../header/JSON_handler.h"
+
 #include "../header/Binning.h"
 #include "../header/BranchMapping.h"
+
 #include "../header/EventStructs_PbPb.h"
-#include "../header/JetSelection_PbPb.h"
 #include "../header/JetStruct.h"
-#include "../header/JetSpectraHistograms.h"
-#include "../header/JetTriggers_2026PbPb_MC.h"
+#include "../header/JetHistograms.h"
+
+#include "../header/JetTriggers_2026PbPb.h"
 // #include "../header/JetTriggers_2025PbPb.h"
 
+// number of the run being processed
+const TString& run_number = "";
+
 // minimum pt of jets to include
-static constexpr Float_t ptcut = 20.0;
+static constexpr Float_t ptcut = 0.0;
 
 // more than number of jets in any event being processed
 static constexpr Int_t maxnref = 150;
@@ -27,29 +35,36 @@ void run(const TString& input_file_list, const TString& output, bool isMC);
 
 int main(int argc, char* argv[]){
     if(argc < 4){
-        std::cerr << "Usage: ./JetHLT <filelist.txt> <output.root> <isMC>" << std::endl;
+        std::cerr << "Usage: ./JetProcessing <filelist.txt> <output.root> <isMC>" << std::endl;
         return 1;
     }
     std::string isMCArg = argv[3];
-    if(isMCArg != "true" && isMCArg != "false" && isMCArg != "1" && isMCArg != "0"){
+    if(isMCArg != "true" && isMCArg != "True" && isMCArg != "false" && isMCArg != "False" && isMCArg != "1" && isMCArg != "0"){
         std::cerr << "ERROR: isMC must be true or false" << std::endl;
         return 1;
     }
-    bool isMC = (isMCArg == "true" || isMCArg == "1");
+    bool isMC = (isMCArg == "true" || isMCArg == "True" || isMCArg == "1");
     run(argv[1], argv[2], isMC);
     return 0;
 }
 
-void JetHLT_SpectraGenerator_PbPb_lxplus(const TString& input_file_list, const TString& output, bool isMC){run(input_file_list, output, isMC);}
+void HiForestJetProcessing(const TString& input_file_list, const TString& output, bool isMC){run(input_file_list, output, isMC);}
  
 void run(const TString& input_file_list, const TString& output, bool isMC){
+    auto start_time = std::chrono::high_resolution_clock::now();
 
     // ttree names
     const Int_t nTTrees = 4;
     TString sTTrees[nTTrees] = {"hiEvtAnalyzer/HiTree", "skimanalysis/HltTree", "akCs4PFJetAnalyzer/t", "hltanalysis/HltTree"};
+ 
+    // L1 object tree name
+    const TString sL1ObjTree = "l1object/L1UpgradeFlatTree";
 
-    // for applying Jet Selections from headers
+    // jet selections
     JetSelect js;
+
+    // JSON handler
+    JSON_handler dcs;
 
     // event objects
     EventStruct evt;
@@ -65,7 +80,7 @@ void run(const TString& input_file_list, const TString& output, bool isMC){
     BinningStruct bins;
 
     // histograms
-    JetSpectraStruct hists(bins);
+    JetHistogramsStruct<maxnref> hists(bins);
 
     // getting list of root files to process
     std::ifstream myfile(input_file_list);
@@ -108,6 +123,11 @@ void run(const TString& input_file_list, const TString& output, bool isMC){
             if(!HLTObjTTrees[t]){throw std::runtime_error(Form("ERROR: Could not find TTree '%s' in file",objTreeName.Data()));}
             SetBranches(HLTObjTTrees[t], trg.HLTObjBranchMap(t));
         }
+ 
+        // getting L1 object ttree
+        TTree* L1ObjTTree = (TTree*)fi->Get(sL1ObjTree);
+        if(!L1ObjTTree){throw std::runtime_error(Form("ERROR: Could not find TTree '%s' in file", sL1ObjTree.Data()));}
+        SetBranches(L1ObjTTree, trg.L1ObjBranchMap());
 
         // looping over events
         Long64_t nentries = ttrees[0]->GetEntries();
@@ -116,20 +136,23 @@ void run(const TString& input_file_list, const TString& output, bool isMC){
             // getting event and event filter info from each event
             ttrees[0]->GetEntry(i);
             ttrees[1]->GetEntry(i);
+            if(!dcs.isGood(evt.run, evt.lumi)){continue;}
 
             // filling event histograms
             hists.vz_unpassed->Fill(evt.vz, evt.w);
-            hists.pclustF->Fill(fltr.pclustF, evt.w);
+            //hists.pclustF->Fill(fltr.pclustF, evt.w);
             hists.ppvF->Fill(fltr.ppvF, evt.w);
             hists.pphfF->Fill(fltr.pphfF, evt.w);
 
             // checking if the event passes event filters and |vz| < 15
-            if((fltr.ppvF==0)||(fltr.pclustF==0)||(fltr.pphfF==0)||(TMath::Abs(evt.vz)>15)){continue;}
+            // if((fltr.ppvF==0)||(fltr.pclustF==0)||(fltr.pphfF==0)||(TMath::Abs(evt.vz)>15)){continue;}
+            if((fltr.ppvF==0)||(fltr.pphfF==0)||(TMath::Abs(evt.vz)>15)){continue;}
 
             // getting jet and trigger ttree info
             ttrees[2]->GetEntry(i);
             ttrees[3]->GetEntry(i);
             for(std::size_t t=0; t<nHLT; t++){HLTObjTTrees[t]->GetEntry(i);}
+            L1ObjTTree->GetEntry(i);
 
             // terminal will yell at me if the leading jet index isn't zero (it never yells at me)
             Int_t lj=0;
@@ -150,6 +173,21 @@ void run(const TString& input_file_list, const TString& output, bool isMC){
             hists.hiBin->Fill(evt.hiBin, evt.w);
             hists.nref->Fill(jt.reco.nref, evt.w);
 
+            const Double_t abseta = TMath::Abs(jt.reco.eta[lj]);
+
+            // looping through all jets in each event
+            for(unsigned int j=0; j<jt.reco.nref; j++){
+
+                // pt cut on jets
+                if(jt.reco.pt[j]<ptcut){continue;}
+                
+                // using header file for jet identification and veto map
+                if(!js.JetSelection(jt.reco.eta[j], jt.reco.phi[j], jt.reco.pf.CEF[j], jt.reco.pf.NEF[j],jt.reco.pf.MUF[j])){continue;}
+                
+                // filling kinematics histogram
+                hists.FillKin(jt.reco, j, evt.hiBin, evt.w);
+            }
+
             // matching HLT object to offline jet
             int iHltMatch[nHLT] = {0};
             for(std::size_t t=0; t<nHLT; t++){
@@ -166,40 +204,56 @@ void run(const TString& input_file_list, const TString& output, bool isMC){
                     Double_t dphi = TMath::ACos(TMath::Cos(obj_phi->at(tj) - jt.reco.phi[lj]));
                     Double_t dR = TMath::Sqrt(deta*deta + dphi*dphi);
 
-                    if((dR<0.3) && (obj_pt->at(tj)>=GetJetTriggerThreshold(sHLTrigs[t]))){
+                    if((dR<0.3) && (obj_pt->at(tj)>=GetJetTriggerThreshold(sHLTrigs[t].name))){
                         iHltMatch[t] = 1;
                         break;
                     }
                 }
             }
 
-            // looping through eta ranges
-            for(std::size_t b=0; b<bins.etaBins.size(); b++){
-                const auto& etaBin = bins.etaBins[b];
-                if((TMath::Abs(jt.reco.eta[lj])<etaBin.lo)||(TMath::Abs(jt.reco.eta[lj])>etaBin.hi)){continue;}
-
-                for(std::size_t hb=0; hb<bins.hiBins.size(); hb++){
-                    const auto& hiBin = bins.hiBins[hb];
-                    if((evt.hiBin<hiBin.lo)||(evt.hiBin>=hiBin.hi)){continue;}
-
-                    hists.ljtpt[b][hb]->Fill(jt.reco.pt[lj], evt.w);
+            // matching L1 jet object to offline leading jet
+            int iL1Match[nL1T] = {0};
+            for(std::size_t t=1; t<nL1T; t++){
+                if(trg.L1T[t]==0){continue;}
+                
+                for(Int_t tj=0; tj<trg.L1_nJets; tj++){
                     
-                    for(std::size_t t=0; t<nHLT; t++){
-                        if(trg.HLT[t]==1){
-                            hists.hlt_ljtpt[JetSpectraStruct::kNoDR][b][t][hb]->Fill(jt.reco.pt[lj], evt.w);
-                            if(iHltMatch[t]==1){hists.hlt_ljtpt[JetSpectraStruct::kDR][b][t][hb]->Fill(jt.reco.pt[lj], evt.w);}
-                        }
-                    }
+                    Double_t deta = TMath::Abs(trg.L1_jetEta[tj] - jt.reco.eta[lj]);
+                    Double_t dphi = TMath::ACos(TMath::Cos(trg.L1_jetPhi[tj] - jt.reco.phi[lj]));
+                    Double_t dR   = TMath::Sqrt(deta*deta + dphi*dphi);
 
-                    // looping through L1Ts
-                    for(std::size_t t=0; t<nL1T; t++){if(trg.L1T[t] == 1){hists.l1_ljtpt[b][t][hb]->Fill(jt.reco.pt[lj], evt.w);}}
+                    if((dR<0.3) && (trg.L1_jetEt[tj] >= GetJetTriggerThreshold(sL1Trigs[t].name))){
+                        iL1Match[t] = 1;
+                        break;
+                    }
                 }
             }
+            
+            for(std::size_t t=0; t<nHLT; t++){
+                if(trg.HLT[t]==1){
+                    hists.FillHLT(t, JetHistogramsStruct<maxnref>::kNoDR, jt.reco.pt[lj], abseta, evt.hiBin, evt.w);
+                    if(iHltMatch[t]==1){
+                        hists.FillHLT(t, JetHistogramsStruct<maxnref>::kDR, jt.reco.pt[lj], abseta, evt.hiBin, evt.w);
+                    }
+                }
+            }
+
+            for(std::size_t t=0; t<nL1T; t++){
+                if(trg.L1T[t]==1){
+                    hists.FillL1T(t, JetHistogramsStruct<maxnref>::kNoDR, jt.reco.pt[lj], abseta, evt.hiBin, evt.w);
+                    if(iL1Match[t]==1){
+                        hists.FillL1T(t, JetHistogramsStruct<maxnref>::kDR, jt.reco.pt[lj], abseta, evt.hiBin, evt.w);
+                    }
+                }
+            }
+
         }
         fi->Close();
     }
+    std::cout << "finished processing files in " << std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - start_time).count() << " seconds" << std::endl;
 
     // making output file and storing histograms
     TFile *fo = new TFile(output,"recreate");
     hists.Write(fo);
+    fo->Close();
 }
