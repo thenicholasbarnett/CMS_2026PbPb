23dadf1c408cf37fdd59156514c53468e96e8fdb
Nicholas Barnett <133896112+thenicholasbarnett@users.noreply.github.com>
2026-06-06 00:44:47 +0200

    Add files via upload

--- header/JetHistograms.h
+++ header/JetHistograms.h
@@ -24,15 +24,19 @@ constexpr std::array<const char*, PFTypes> PFTypeTitles = {"Charged Hadron Fract
 template <Int_t MAXNREF>
 struct JetHistogramsStruct{
 
-    // Axis indices for hn_kin: {pt, |eta|, phi, hiBin, pfFrac, pfType}
+    // Axis indices for kin: {pt, |eta|, phi, hiBin, chf, nhf, cef, nef, muf}
     enum KinAxis{
         kKin_pt = 0,
         kKin_eta = 1,
         kKin_phi = 2,
         kKin_hiBin = 3,
-        kKin_pfFrac = 4,
-        kKin_pfType = 5
+        kKin_CHF = 4,
+        kKin_NHF = 5,
+        kKin_CEF = 6,
+        kKin_NEF = 7,
+        kKin_MUF = 8
     };
+    static constexpr KinAxis PFTypeToAxis(Int_t p){return static_cast<KinAxis>(kKin_CHF + p);}
 
     enum MatchType{
         kNoDR = 0, // no offline-HLT object matching
@@ -40,7 +44,7 @@ struct JetHistogramsStruct{
         kNMatchTypes = 2
     };
     
-    // Axis indices for hn_L1T: {pt, |eta|, hiBin, L1T index, matchType}
+    // Axis indices for L1T: {pt, |eta|, hiBin, L1T index, matchType}
     enum L1TAxis{
         kL1T_pt = 0,
         kL1T_eta = 1,
@@ -49,7 +53,7 @@ struct JetHistogramsStruct{
         kL1T_matchType = 4
     };
  
-    // Axis indices for hn_HLT: {pt, |eta|, hiBin, HLT index, matchType}
+    // Axis indices for HLT: {pt, |eta|, hiBin, HLT index, matchType}
     enum HLTAxis {
         kHLT_pt = 0,
         kHLT_eta = 1,
@@ -70,9 +74,9 @@ struct JetHistogramsStruct{
     TH1I* pphfF = nullptr;
 
     // higher dimensional histograms
-    THnSparseF* hn_kin = nullptr;
-    THnSparseF* hn_L1T = nullptr;
-    THnSparseF* hn_HLT = nullptr;
+    THnSparseF* kin = nullptr;
+    THnSparseF* L1T = nullptr;
+    THnSparseF* HLT = nullptr;
 
     // constructors
     JetHistogramsStruct(const BinningStruct& bins){InitHistograms(bins);}
@@ -90,48 +94,44 @@ struct JetHistogramsStruct{
         ppvF = MakeTH1<TH1I>("hppvF", bins.trig);
         pphfF = MakeTH1<TH1I>("hpphfF", bins.trig);
 
-        const AxisBins pfTypeAxis = {PFTypes, 0.0, (Float_t)PFTypes, "PF type"};
-        hn_kin = MakeTHnSparse<THnSparseF>(
-            "hn_kin", "Jet kinematics and PF fractions",
-            {bins.pt, bins.abseta, bins.phi, bins.hiBin, bins.pfFrac, pfTypeAxis}
+        kin = MakeTHnSparse<THnSparseF>(
+            "kin", "Jet kinematics and PF fractions",
+            {bins.pt, bins.abseta, bins.phi, bins.hiBin,bins.pfFrac, bins.pfFrac, bins.pfFrac, bins.pfFrac, bins.pfFrac}
         );
+        for(Int_t p = 0; p < PFTypes; p++){kin->GetAxis(PFTypeToAxis(p))->SetTitle(PFTypeNames[p]);}
 
-        hn_L1T = MakeTHnSparse<THnSparseF>("hn_L1T", "L1T leading jet p_{T}",{bins.pt,bins.abseta,bins.hiBin,{(Int_t)nL1T, 0.0, (Float_t)nL1T, "L1T index"},{(Int_t)kNMatchTypes, 0.0, (Float_t)kNMatchTypes, "match type"}});
-        hn_HLT = MakeTHnSparse<THnSparseF>("hn_HLT", "HLT leading jet p_{T}",{bins.pt,bins.abseta,bins.hiBin,{(Int_t)nHLT, 0.0, (Float_t)nHLT, "HLT index"},{(Int_t)kNMatchTypes, 0.0, (Float_t)kNMatchTypes, "match type"}});  
+        L1T = MakeTHnSparse<THnSparseF>("hn_L1T", "L1T leading jet p_{T}",{bins.pt,bins.abseta,bins.hiBin,{(Int_t)nL1T, 0.0, (Float_t)nL1T, "L1T index"},{(Int_t)kNMatchTypes, 0.0, (Float_t)kNMatchTypes, "match type"}});
+        HLT = MakeTHnSparse<THnSparseF>("hn_HLT", "HLT leading jet p_{T}",{bins.pt,bins.abseta,bins.hiBin,{(Int_t)nHLT, 0.0, (Float_t)nHLT, "HLT index"},{(Int_t)kNMatchTypes, 0.0, (Float_t)kNMatchTypes, "match type"}});  
 
-        for(std::size_t t=0; t<nL1T; t++){hn_L1T->GetAxis(kL1T_trig)->SetBinLabel(t+1, GetL1ShortName(sL1Trigs[t].name));}
-        for(std::size_t t=0; t<nHLT; t++){hn_HLT->GetAxis(kHLT_trig)->SetBinLabel(t+1, GetHLTShortName(sHLTrigs[t].name));}
-        hn_HLT->GetAxis(kHLT_matchType)->SetBinLabel(kNoDR+1, "noDR");
-        hn_HLT->GetAxis(kHLT_matchType)->SetBinLabel(kDR+1, "dR");
-        hn_L1T->GetAxis(kL1T_matchType)->SetBinLabel(kNoDR+1, "noDR");
-        hn_L1T->GetAxis(kL1T_matchType)->SetBinLabel(kDR+1, "dR");
-        for(Int_t p=0; p<PFTypes; p++){hn_kin->GetAxis(kKin_pfType)->SetBinLabel(p+1, PFTypeNames[p]);}
+        for(std::size_t t=0; t<nL1T; t++){L1T->GetAxis(kL1T_trig)->SetBinLabel(t+1, GetL1ShortName(sL1Trigs[t].name));}
+        for(std::size_t t=0; t<nHLT; t++){HLT->GetAxis(kHLT_trig)->SetBinLabel(t+1, GetHLTShortName(sHLTrigs[t].name));}
+        HLT->GetAxis(kHLT_matchType)->SetBinLabel(kNoDR+1, "noDR");
+        HLT->GetAxis(kHLT_matchType)->SetBinLabel(kDR+1, "dR");
+        L1T->GetAxis(kL1T_matchType)->SetBinLabel(kNoDR+1, "noDR");
+        L1T->GetAxis(kL1T_matchType)->SetBinLabel(kDR+1, "dR");
     }
 
     // filling functions
-    
+
     void FillKin(const typename JetStruct<MAXNREF>::RecoMomenta& reco, Int_t j, Int_t hiBinVal, Double_t w = 1.0){
-        const Float_t fracs[PFTypes] = {reco.pf.CHF[j], reco.pf.NHF[j],reco.pf.CEF[j], reco.pf.NEF[j], reco.pf.MUF[j]};
-        for(Int_t p=0; p<PFTypes; p++){
-            Double_t x[6] = {reco.pt[j], TMath::Abs(reco.eta[j]), reco.phi[j],(Double_t)hiBinVal, fracs[p], p + 0.5};
-            hn_kin->Fill(x, w);
-        }
+        Double_t x[9] = {reco.pt[j], TMath::Abs(reco.eta[j]), reco.phi[j], (Double_t)hiBinVal,reco.pf.CHF[j], reco.pf.NHF[j], reco.pf.CEF[j], reco.pf.NEF[j], reco.pf.MUF[j]};
+        kin->Fill(x, w);
     }
 
     void FillL1T(std::size_t iL1T, MatchType matchType, Double_t pt,Double_t abseta, Int_t hiBin, Double_t w = 1.0){
         Double_t x[5] = {pt, abseta, (Double_t)hiBin, (Double_t)iL1T + 0.5, (Double_t)matchType + 0.5};
-        hn_L1T->Fill(x, w);
+        L1T->Fill(x, w);
     }
 
     void FillHLT(std::size_t iHLT, MatchType matchType, Double_t pt, Double_t abseta, Int_t hiBin, Double_t w = 1.0){
         Double_t x[5] = {pt, abseta, (Double_t)hiBin, (Double_t)iHLT + 0.5, (Double_t)matchType + 0.5};
-        hn_HLT->Fill(x, w);
+        HLT->Fill(x, w);
     }
 
     // projection helpers
 
     TH1D* ProjectL1T_pt(std::size_t iL1T, MatchType matchType, Double_t etalo, Double_t etahi, Int_t hiBinlo, Int_t hiBinhi, const TString& suffix = "") const{
-        return ProjectTHnSparse1D(hn_L1T, kL1T_pt,
+        return ProjectTHnSparse1D(L1T, kL1T_pt,
             {
                 {kL1T_eta, etalo, etahi},
                 {kL1T_hiBin, (Double_t)hiBinlo, (Double_t)hiBinhi},
@@ -141,7 +141,7 @@ struct JetHistogramsStruct{
     }
  
     TH1D* ProjectHLT_pt(std::size_t iHLT, MatchType matchType, Double_t etalo, Double_t etahi, Int_t hiBinlo, Int_t hiBinhi, const TString& suffix = "") const {
-        return ProjectTHnSparse1D(hn_HLT, kHLT_pt,
+        return ProjectTHnSparse1D(HLT, kHLT_pt,
             {
                 {kHLT_eta, etalo, etahi},
                 {kHLT_hiBin, (Double_t)hiBinlo, (Double_t)hiBinhi},
@@ -159,9 +159,9 @@ struct JetHistogramsStruct{
         // WriteAll(pclustF);
         WriteAll(ppvF);
         WriteAll(pphfF);
-        if(hn_kin) hn_kin->Write();
-        if(hn_L1T) hn_L1T->Write();
-        if(hn_HLT) hn_HLT->Write();
+        if(kin) kin->Write();
+        if(L1T) L1T->Write();
+        if(HLT) HLT->Write();
     }
 };
 #endif
